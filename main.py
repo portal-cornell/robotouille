5553221002505444f3ea8db8daaf6d8547fb75b7
chalo2000 <gg387@cornell.edu>
2023-04-28 13:52:48 -0400

    Fix inconsistent IDing and add seed argument

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--environment_name", help="The name of the environment to create.", default="original")
+parser.add_argument("--seed", help="The seed to use for the environment.", default=0)
 args = parser.parse_args()
 
-seed = 0
 noisy_randomization = True
-env = overcooked_utils.create_overcooked_env(args.environment_name, seed, noisy_randomization)
+env = overcooked_utils.create_overcooked_env(args.environment_name, args.seed, noisy_randomization)
 obs, info = env.reset()
 env.render(mode='human')
 done = False
