161781875a487339f9add17e8d6897efe7f678da
Yash Sharma <ys749@cornell.edu>
2023-10-31 18:27:45 -0400

    converts main.py into an importable simulator

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import robotouille.robotouille_env as robotouille_env
+from robotouille import robotouille_env, simulator
 import utils.robotouille_input as robotouille_input
 import argparse
 import pygame
@@ -9,22 +9,4 @@ parser.add_argument("--seed", help="The seed to use for the environment.", defau
 parser.add_argument("--noisy_randomization", action="store_true", help="Whether to use 'noisy randomization' for procedural generation")
 args = parser.parse_args()
 
-env, json, renderer = robotouille_env.create_robotouille_env(args.environment_name, args.seed, args.noisy_randomization)
-obs, info = env.reset()
-env.render(mode='human')
-done = False
-interactive = False # Set to True to interact with the environment through terminal REPL (ignores input)
-
-while not done:
-    # Construct action from input
-    pygame_events = pygame.event.get()
-    # Mouse clicks for movement and pick/place stack/unstack
-    mousedown_events = list(filter(lambda e: e.type == pygame.MOUSEBUTTONDOWN, pygame_events))
-    # Keyboard events ('e' button) for cut/cook ('space' button) for noop
-    keydown_events = list(filter(lambda e: e.type == pygame.KEYDOWN, pygame_events))
-    action = robotouille_input.create_action_from_control(env, obs, mousedown_events+keydown_events, renderer)
-    if not interactive and action is None:
-        # Retry for keyboard input
-        continue
-    obs, reward, done, info = env.step(action=action, interactive=interactive)
-    env.render(mode='human')
+simulator(args.environment_name, args.seed, args.noisy_randomization)
---
+++ robotouille/robotouille_simulator.py
@@ -0,0 +1,26 @@
+import pygame
+from utils.robotouille_input import create_action_from_control
+from robotouille.robotouille_env import create_robotouille_env
+
+
+def simulator(environment_name: str, seed, noisy_randomization):
+    # Your code for robotouille goes here
+    env, json, renderer = create_robotouille_env(environment_name, seed, noisy_randomization)
+    obs, info = env.reset()
+    env.render(mode='human')
+    done = False
+    interactive = False # Set to True to interact with the environment through terminal REPL (ignores input)
+
+    while not done:
+        # Construct action from input
+        pygame_events = pygame.event.get()
+        # Mouse clicks for movement and pick/place stack/unstack
+        mousedown_events = list(filter(lambda e: e.type == pygame.MOUSEBUTTONDOWN, pygame_events))
+        # Keyboard events ('e' button) for cut/cook ('space' button) for noop
+        keydown_events = list(filter(lambda e: e.type == pygame.KEYDOWN, pygame_events))
+        action = create_action_from_control(env, obs, mousedown_events+keydown_events, renderer)
+        if not interactive and action is None:
+            # Retry for keyboard input
+            continue
+        obs, reward, done, info = env.step(action=action, interactive=interactive)
+        env.render(mode='human')
