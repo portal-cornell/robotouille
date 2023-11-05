1aadcedaf4a8d5e7cf0caba2eb9af9e04fb4c2bc
Yash Sharma <ys749@cornell.edu>
2023-11-05 17:17:27 -0500

    removed extra environment that was causing re-registration of robotouille environment

--- robotouille/__init__.py
+++ robotouille/__init__.py
@@ -1,4 +1,2 @@
 from robotouille.robotouille_env import RobotouilleRenderer, create_robotouille_env
 from robotouille.robotouille_simulator import simulator
-
-lettuce_burger_env = create_robotouille_env("high_level_lettuce_burger")
