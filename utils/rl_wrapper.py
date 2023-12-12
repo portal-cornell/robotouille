ee9d5811f08a9e478ee97f5bda7337a029290c83
Daniel Cao <55444333+deuterium1729@users.noreply.github.com>
2023-12-12 14:11:41 -0500

    add wandb

--- utils/rl_wrapper.py
+++ utils/rl_wrapper.py
@@ -6,6 +6,8 @@ import utils.robotouille_utils as robotouille_utils
 import utils.pddlgym_utils as pddlgym_utils
 import utils.robotouille_wrapper as robotouille_wrapper
 from utils.rl_env import RLEnv
+import wandb
+wandb.login()
 
 
 class RLWrapper(robotouille_wrapper.RobotouilleWrapper):
@@ -15,6 +17,26 @@ class RLWrapper(robotouille_wrapper.RobotouilleWrapper):
         self.pddl_env = env
         self.env = None
         self.max_steps = 40
+
+        # Configuration dictionary for tracking metrics
+        self.metrics_config = {
+            "ep_rew_mean": None,  # Mean episode reward
+            "total_timesteps": 0,    # Total number of timesteps
+            "iterations": 0,         # Number of iterations
+            "ep_len_mean": None      # Mean episode length
+        }
+        # Initialize WandB with the metrics config
+        wandb.init(project="6756-rl-experiments", config=self.metrics_config)
+    
+    def log_metrics(self, update_dict):
+        """
+        Log metrics to the metrics_config and to WandB.
+        :param update_dict: A dictionary containing updates to the metrics.
+        """
+        # Update the metrics configuration with new values
+        self.metrics_config.update(update_dict)
+        # Log the updated metrics to WandB
+        wandb.log(self.metrics_config)
 
     def _wrap_env(self):
         expanded_truths, expanded_states = pddlgym_utils.expand_state(
@@ -35,12 +57,12 @@ class RLWrapper(robotouille_wrapper.RobotouilleWrapper):
 
     def step(self, action=None, interactive=False, debug=False):
         action = self.env.unwrap_move(action)
-
         if debug:
             print(action)
         if action not in self.env.valid_actions:
             obs, reward, done, info = self.pddl_env.prev_step
-            reward -= 200000
+            print("Not a valid move ", action)
+            reward -= 1
             self.pddl_env.prev_step = (obs, reward, done, info)
             self.pddl_env.timesteps += 1
 
@@ -48,8 +70,11 @@ class RLWrapper(robotouille_wrapper.RobotouilleWrapper):
         else:
             action = str(action)
             obs, reward, done, info = self.pddl_env.step(action, interactive)
-            reward += 100
+            print("This is valid move", action)
+            reward += 1
             self.pddl_env.prev_step = (obs, reward, done, info)
+        
+        wandb.log({'reward per step':reward})
         self._wrap_env()
 
         return (
