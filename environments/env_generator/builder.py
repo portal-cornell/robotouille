65c7556fed686ab0a1a45217ecd3fe2b4f571025
Brian La <brianyla@gmail.com>
2024-05-20 15:23:33 -0700

    Fix moveitem bug

--- environments/env_generator/builder.py
+++ environments/env_generator/builder.py
@@ -265,7 +265,7 @@ def build_stacking_predicates(environment_dict):
 
 def build_action_predicates(environment_dict):
     predicates_str = ""
-    actions = ["cook", "cut", "place", "move", "fry", "frycut"]
+    actions = ["cook", "cut", "moveitem", "move", "fry", "frycut"]
     for player in environment_dict["players"]:
         for action in actions:
             if action in player["actions"]:
