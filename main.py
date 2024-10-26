9fdd2c520cac043aaf82aee1f615fb19d6fa1877
lsuyean <131217723+lsuyean@users.noreply.github.com>
2024-10-26 00:10:54 -0400

    Movement (#31)
    * rough draft of creation effect
    
    * implemented pickup and place container
    
    * implemented fill water
    
    * Made boil water: bug when creating new objects, no id assigned
    
    * boil soup bug fixed
    
    * finish implementing all actions
    
    * implemented cook soup test
    
    * added conditional delayed effect for cook, boil, and fry; added versions to env files; empty fields in problem files not not required
    
    * updated based on comments
    
    * edited goal such that it can include objects not in initial state
    
    * pddl functions uncommented
    
    * fixed bug for station location predicates and no conditional effect for filling water
    
    * Implemented multi-agent
    
    * addressed comments, fixed bug in creation effect
    
    * fixed tests
    
    * made changes based on comments
    
    * initial implementation
    
    * fixed movement
    
    * fixed issues in simulator in collecting actions from correct players
    
    * path finding ensures that players cannot move to destination of other players
    
    * Enhanced Goals (#29)
    
    * rough draft of creation effect
    
    * implemented pickup and place container
    
    * implemented fill water
    
    * Made boil water: bug when creating new objects, no id assigned
    
    * boil soup bug fixed
    
    * finish implementing all actions
    
    * implemented cook soup test
    
    * added conditional delayed effect for cook, boil, and fry; added versions to env files; empty fields in problem files not not required
    
    * updated based on comments
    
    * edited goal such that it can include objects not in initial state
    
    * pddl functions uncommented
    
    * fixed bug for station location predicates and no conditional effect for filling water
    
    * addressed comments, fixed bug in creation effect
    
    * fixed tests
    
    * temporary commit
    
    * edited based on comments; reworked movement class and added station class
    
    * used pygame clock for animations, cleaned up code
    
    * Updated based on comments
    
    * fixed based on comments
    
    * made edits
    
    * fixed merge issues when rendering containers
    
    * fixed rendering bug

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--environment_name", help="The name of the environment to create.", default="original")
 parser.add_argument("--seed", help="The seed to use for the environment.", default=None)
 parser.add_argument("--noisy_randomization", action="store_true", help="Whether to use 'noisy randomization' for procedural generation")
+parser.add_argument("--movement_mode", help="The movement mode to use for the environment.", default="traverse")
 args = parser.parse_args()
 
-simulator(args.environment_name, args.seed, args.noisy_randomization)
+simulator(args.environment_name, args.seed, args.noisy_randomization, args.movement_mode)
