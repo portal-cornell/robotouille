ebd84cdd944a9b1cda9d6160463618ca6eccdda6
Colin Wu <45516888+CollinWoo@users.noreply.github.com>
2024-11-21 01:09:36 -0500

    add level editor screen template

---
+++ frontend/level_editor.py
@@ -0,0 +1,77 @@
+import pygame
+from frontend.constants import *
+from frontend.image import Image
+from frontend.textbox import Textbox
+from frontend.screen import ScreenInterface
+from frontend.loading import LoadingScreen
+
+# Set up the assets directory
+ASSETS_DIRECTORY = os.path.abspath(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "assets", "frontend", "matchmaking"))
+
+
+class LevelEditorScreen(ScreenInterface):
+    def __init__(self, window_size):
+        """
+        Initialize the Lobby Screen.
+
+        Args:
+            window_size (tuple): (width, height) of the window
+        """
+        super().__init__(window_size)
+        self.background = Image(self.screen, self.background_image, 0.5, 0.5, self.scale_factor, anchor="center")
+        self.players = [
+        ]
+        self.host = False
+        self.count = 0
+
+    def load_assets(self):
+        """Load necessary assets."""
+        background_path = os.path.join(SHARED_DIRECTORY, "background.png")
+        empty_path = os.path.join(ASSETS_DIRECTORY, "empty.png")
+        profile_path = os.path.join(ASSETS_DIRECTORY, "profile.png")
+
+        self.background_image = LoadingScreen.ASSET[background_path]
+        self.empty_profile_image = LoadingScreen.ASSET[empty_path]
+        self.profile_image = LoadingScreen.ASSET[profile_path]
+
+    def setPlayers(self, existing_players):
+        """
+        Set existing players.
+
+        Args:
+            existing_players (list of dict): List of players to add, each with 'name' and 'icon'.
+        """
+
+        if len(existing_players) >= MAX_PLAYERS:
+            raise Exception("To many players")
+
+        self.count = len(existing_players)
+
+        for i in range(4):
+            if i < len(existing_players):
+                self.players[i]["name"].set_text(existing_players[i])
+                self.players[i]["icon"].set_image(self.profile_image)
+            else:
+                self.players[i]["name"].set_text("")
+                self.players[i]["icon"].set_image(self.empty_profile_image)
+
+    def draw(self):
+        """Draws all the screen components."""
+        self.background.draw()
+        for player in self.players:
+            player["icon"].draw()
+            if player["name"].get_text(): player["name"].draw()
+
+        # draw play button if player is host
+
+    def update(self):
+        """Update the screen and handle events."""
+        super().update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    self.set_next_screen(MAIN_MENU)
+                elif event.key == pygame.K_g:
+                    self.set_next_screen(GAME)
