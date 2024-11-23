9e0eeb2cf771703711d83e3e09734369e30b5866
Ian Urquhart <iju2@cornell.edu>
2024-11-23 06:55:57 -0500

    fixed issue where egg is fried off pan

--- environments/env_generator/object_enums.py
+++ environments/env_generator/object_enums.py
@@ -1,5 +1,6 @@
 from enum import Enum
 
+
 class Item(Enum):
     BOTTOMBUN = "bottombun"
     LETTUCE = "lettuce"
@@ -13,10 +14,11 @@ class Item(Enum):
     POTATO = "potato"
     EGG_YOLK = "egg_yolk"
     EGG = "egg"
-    PAN = "pan"
+
 
 class Player(Enum):
     ROBOT = "robot"
+
 
 class Station(Enum):
     BOARD = "board"
@@ -27,18 +29,29 @@ class Station(Enum):
     COUNTER = "counter"
     BLENDER = "blender"
 
+
 class Container(Enum):
     POT = "pot"
     BOWL = "bowl"
     EGG_CARTON = "egg_carton"
     BLENDERCUP = "blendercup"
+    PAN = "pan"
+
 
 class Meal(Enum):
     WATER = "water"
     BOILING_WATER = "boiling_water"
     SOUP = "soup"
 
-TYPES = {"item": Item, "player": Player, "station": Station, "container": Container, "meal": Meal}
+
+TYPES = {
+    "item": Item,
+    "player": Player,
+    "station": Station,
+    "container": Container,
+    "meal": Meal,
+}
+
 
 def str_to_typed_enum(s):
     """
@@ -46,10 +59,10 @@ def str_to_typed_enum(s):
 
     Args:
         s (str): String to convert.
-    
+
     Raises:
         ValueError: If the string cannot be converted into any of the typed enums.
-    
+
     Returns:
         typed_enum (Enum): Enum of the string.
     """
