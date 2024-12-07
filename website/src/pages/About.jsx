c69e67bbded0856eff81f7289f170e4842a2092e
Amelia Zheng <ayz23@cornell.edu>
2024-12-07 00:19:09 -0500

    update about page layout

--- website/src/pages/About.jsx
+++ website/src/pages/About.jsx
@@ -6,50 +6,50 @@ import lettucetomatoburger from "../assets/lettuce_tomato_burger.gif";
 
 const About = () => {
   return (
-    <div className="bg-primary-darkRed text-white font-roboto-slab rounded-lg shadow-2xl max-w-4xl mx-auto my-10 p-4 mt-20">
+    <div className="relative mt-28">
       {/* Section Header */}
-      <div className="flex justify-center -translate-y-20">
+      <div className="absolute inset-x-0 -top-15 flex justify-center">
         <img
           src={aboutHeader}
           alt="About Header"
-          className="w-1/2 h-auto rounded-t-lg"
+          className="w-1/3 h-auto rounded-t-lg"
         />
       </div>
 
-      {/* About Text */}
-      <div className="p-6">
-        <h2 className="text-lg font-semibold text-left mb-4">
-          About Robotouille
-        </h2>
-        <p className="text-white text-left">
-          Robots will be involved in every part of our lives in the near future
-          so we need to teach them how to perform complex tasks. Humans break
-          apart complex tasks like making hamburgers into smaller subtasks like
-          cutting lettuce and cooking patties. We can teach robots to do the
-          same by showing them how to perform easier tasks subtasks and then
-          combine those subtasks to perform harder tasks. We created Robotouille
-          so we can stress test this idea through an easily customizable cooking
-          environment where the task possibilities are endless!
-        </p>
-      </div>
+      {/* Main Content */}
+      <div className="bg-primary-darkRed text-white font-roboto-slab rounded-lg shadow-2xl max-w-3xl mx-auto mb-12 p-6">
+        <div>
+          <p className="text-white text-left mt-6">
+            Robots will be involved in every part of our lives in the near
+            future so we need to teach them how to perform complex tasks. Humans
+            break apart complex tasks like making hamburgers into smaller
+            subtasks like cutting lettuce and cooking patties. We can teach
+            robots to do the same by showing them how to perform easier tasks
+            subtasks and then combine those subtasks to perform harder tasks. We
+            created Robotouille so we can stress test this idea through an
+            easily customizable cooking environment where the task possibilities
+            are endless!
+          </p>
+        </div>
 
-      {/* GIF Section */}
-      <div className="flex justify-between items-center gap-4 p-6">
-        <img
-          src={cheeseBurger}
-          alt="Cheeseburger"
-          className="w-1/3 rounded-lg shadow-lg"
-        />
-        <img
-          src={kitchen}
-          alt="Kitchen"
-          className="w-1/3 rounded-lg shadow-lg"
-        />
-        <img
-          src={lettucetomatoburger}
-          alt="Lettuce Tomato Burger"
-          className="w-1/3 rounded-lg shadow-lg"
-        />
+        {/* GIFs */}
+        <div className="flex justify-center items-cente gap-6 p-6">
+          <img
+            src={cheeseBurger}
+            alt="Cheeseburger"
+            className="w-1/3 rounded-lg shadow-lg"
+          />
+          <img
+            src={kitchen}
+            alt="Kitchen"
+            className="w-1/3 rounded-lg shadow-lg"
+          />
+          <img
+            src={lettucetomatoburger}
+            alt="Lettuce Tomato Burger"
+            className="w-1/3 rounded-lg shadow-lg"
+          />
+        </div>
       </div>
     </div>
   );
